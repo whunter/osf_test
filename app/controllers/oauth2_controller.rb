b4e547a92740d5c4222d5a4d22633b7ed823c3e4
Lee Hunter <whunter@vt.edu>
2017-04-28 14:01:15 -0400

    redirect print out code

--- app/controllers/oauth2_controller.rb
+++ app/controllers/oauth2_controller.rb
@@ -6,15 +6,23 @@ class Oauth2Controller < ApplicationController
     client = OAuth2::Client.new(
       Rails.application.secrets['osf_client_id'],
       Rails.application.secrets['osf_client_secret'],
-      :site => 'https://accounts.osf.io'
+      :site => 'https://accounts.osf.io',
     )
-    @auth_url = client.auth_code.authorize_url(:redirect_uri => "#{request.base_url}/oauth2/callback")
+    @auth_url = client.auth_code.authorize_url(
+      :redirect_uri => "#{request.base_url}/oauth2/callback",
+      :scope => 'osf.full_read',
+      :response_type => 'code',
+      :state => 'iuasdhf734t9hiwlf7'
+    )
     @auth_url = @auth_url.gsub(/oauth2?/, "oauth2")
+    redirect_to @auth_url
   end
 
   def token
   end
 
   def callback
+    code = params['code']
+    raise code.inspect
   end
 end
